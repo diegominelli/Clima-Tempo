1598eecdb45b4960efaef94da53bf4c26c4a968d
Diego Minelli <dodi@gft.com>
2021-08-30 18:37:08 -0300

    Criando funcao para limpar o resultado

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
   let input = document.querySelector("#searchInput").value;
 
   if (input !== "") {
+    clearInfo();
     showWarning("Carregando...");
 
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
@@ -23,8 +24,11 @@ document.querySelector(".busca").addEventListener("submit", async (event) => {
         windAngle: json.wind.deg,
       });
     } else {
+      clearInfo();
       showWarning("Não encontramos essa localização.");
     }
+  } else {
+    clearInfo();
   }
 });
 
@@ -32,6 +36,26 @@ function showInfo(json) {
   showWarning("");
 
   document.querySelector(".resultado").style.display = "block";
+
+  document.querySelector(".titulo").innerHTML = `${json.name}, ${json.country}`;
+  document.querySelector(".tempInfo").innerHTML = `${json.temp} <sup>°C</sup>`;
+  document.querySelector(
+    ".ventoInfo"
+  ).innerHTML = `${json.windSpeed} <span>km/h</span>`;
+  document
+    .querySelector(".temp img")
+    .setAttribute(
+      "src",
+      `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`
+    );
+  document.querySelector(".ventoPonto").style.transform = `rotate(${
+    json.windAngle - 90
+  }deg)`;
+}
+
+function clearInfo() {
+  showWarning("");
+  document.querySelector(".resultado").style.display = "none";
 }
 
 function showWarning(msg) {
